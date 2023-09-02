12c7a899b8538e495211a6bba06b6919e85c4c06
app-wizard <>
2023-09-02 21:38:43 +0000

    add integer validator

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('cred.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -16,28 +16,30 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 # data = sales.get_all_values()
 # print(data)
 
+
 def get_sales_data():
     """
     Get data
     """
-    print ("Please enter sales data -")
-    print ("Example: 10,20,30,40,50,60\n")
+    print("Please enter sales data")
+    print("Example: 10,20,30,40,50,60\n")
     data_str = input("Enter your data here: ")
 
     sales_data = data_str.split(",")
     validate_data(sales_data)
+
 
 def validate_data(values):
     """
     input validator
     """
     try:
-        if len(values) !=6:
+        [int(value) for value in values]
+        if len(values) != 6:
             raise ValueError(
-        f"Exaxly 6 values required. You provided {len(values)} values"
-        )
+                f"Exaxly 6 values required. You provided {len(values)} values")
     except ValueError as e:
-        print(f"Invalid data {e} /n") 
-    
+        print(f"Invalid data {e} \n")
+
 
 get_sales_data()
