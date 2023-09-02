b525fdddf2d7936b67067514e40f49219eb5900d
app-wizard <>
2023-09-02 21:23:05 +0000

    add validator function

--- run.py
+++ run.py
@@ -23,6 +23,21 @@ def get_sales_data():
     print ("Please enter sales data -")
     print ("Example: 10,20,30,40,50,60\n")
     data_str = input("Enter your data here: ")
-    print (f"The data provided {data_str}")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    input validator
+    """
+    try:
+        if len(values) !=6:
+            raise ValueError(
+        f"Exaxly 6 values required. You provided {len(values)} values"
+        )
+    except ValueError as e:
+        print(f"Invalid data {e} /n") 
+    
 
 get_sales_data()
